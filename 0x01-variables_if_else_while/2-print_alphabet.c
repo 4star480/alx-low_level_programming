355dee69af031ea21210ad45fc78bf1d84c26bba
4star480 <abuhumayl@gmail.com>
2022-07-03 06:07:21 -0700

    C variables

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,2 +1,15 @@
-#!/bin/bash
-export PATH="$PATH:/action"
+#include <stdio.h>
+
+/**
+* main - Prints the alphabet in lowercase.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
+}
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,2 +1,16 @@
- #!/bin/bash
-echo $PATH | tr -s ":" "\n" | wc -l
+#include <stdio.h>
+/**
+* main - Prints the alphabet in lowercase, and then in uppercase.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+for (letter = 'A'; letter <= 'Z'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
+}
